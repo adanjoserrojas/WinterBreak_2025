5a346969d7db325946c28b87e222da80af3f0767
adanjoserrojas <adanrojas224@gmail.com>
2026-03-02 07:46:54 -0500

    Practice#1

---
+++ Practice#1.py
@@ -0,0 +1,32 @@
+
+input = ["u12:/api/orders",
+         "u2:/api/profile",
+         "u3:/api/education",
+         "u4:/api/profile",
+         "u1:/api/main",
+         "u1:/api/main"]
+
+end_marker = ":"
+
+def userIdRequest(input, m) -> int:
+
+    endpoints = {}
+    returnStrings = []
+
+    for i in range(len(input)):
+
+        string = input[i]
+        userId, path = string.split(":", 1)
+        endpoints[path] = endpoints.get(path, set())
+        endpoints[path].add(userId)
+
+        if len(endpoints[path]) >= m and path not in returnStrings: returnStrings.append(path)
+
+    return returnStrings
+
+def main():
+    strings = userIdRequest(input, 2)
+    print(strings)
+
+if "__main__" == __name__:
+    main()
